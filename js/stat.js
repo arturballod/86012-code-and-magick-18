f90953df82321e042084df01cd55b321c2fecd57
nokia6680 <zenitfan@yandex.ru>
2019-09-17 20:42:32 +0200

    Частично

--- js/stat.js
+++ js/stat.js
@@ -4,66 +4,90 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+// Отступ между текстом и границами
+var GAP = 10;
 // Ширина колонки
 var BAR_WIDTH = 40;
 // Расстояние между колонками
 var BAR_GAP = 50;
-// Отступ между текстом и границами
-var GAP = 10;
-// Margin
+// Горизонтальный отступ гистограммы от границы облака
 var MARGIN = 55;
 // высота текста
-var TEXT_HEIGHT = 15;
-// отступ текста
+var FONT_HEIGHT = 15;
+// Отступ от текста
 var FONT_GAP = 10;
+// Цвет бара и текста гл.игрока
+var MY_COLOR = 'rgba(250, 0, 0, 1)';
 // Позиция текста вертикальная
 var textY = CLOUD_HEIGHT - GAP;
+// "Шаг" бара гистограммы по горизонтали
+var barStep = BAR_WIDTH + BAR_GAP;
+// Позиция бара гистограммы по горизонтали
+var barX = CLOUD_X + MARGIN;
 // Позиция бара вертикальная
-var barY = CLOUD_HEIGHT - GAP - TEXT_HEIGHT - FONT_GAP / 2;
+var barY = CLOUD_HEIGHT - GAP - FONT_HEIGHT - FONT_GAP / 2;
 // Расчет максимальной высоты гистограммы
-var barHeight = CLOUD_HEIGHT - GAP * 2 - (FONT_GAP + TEXT_HEIGHT) * 4;
+var barHeight = CLOUD_HEIGHT - GAP * 2 - (FONT_GAP + FONT_HEIGHT) * 4;
+// Позиция поздравления горизонтальная
+var titleX = CLOUD_X + (GAP * 3);
+// Позиция поздравления вертикальная
+var titleY = CLOUD_Y + (GAP * 3);
+// Текст заголовка при победе
+var titleText = 'Ура вы победили!';
+// Текст подзаголовка при победе
+var subTitleText = 'Список победителей:';
+
 
 // Функция для рендера облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
-// Сортировка
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
+// Функция поиска максимального значения
+function getMaxElement(arr) {
+  return Math.max.apply(null, arr);
+}
+// Функция генерации рандомной насыщенности цвета
+var makeRandomColor = function () {
+  return ('hsl(240, ' + (Math.random() * 100) + '%, 50%)');
+};
 
-  return maxElement;
+// Функция отрисовки текста поздравления
+var renderTitle = function (ctx, x, y, color, font) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(titleText, x, y);
+};
+// Функция отрисовки текста вывода списка победителей
+var renderSubtitle = function (ctx, x, y, color, font) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.fillText(subTitleText, x, y);
+};
+var renderHist = function (ctx, players, times) {
+  var maxTime = getMaxElement(times);
+
+  players.forEach(function (name, i) {
+    var barColor = name === 'Вы' ? MY_COLOR : makeRandomColor();
+    var barFinalHeight = (barHeight * times[i]) / maxTime;
+    var barPos = barX + (barStep * i);
+    var counterY = CLOUD_HEIGHT - (barFinalHeight + GAP + FONT_HEIGHT + FONT_GAP);
+
+    ctx.fillStyle = barColor;
+    ctx.fillRect(barPos, barY, BAR_WIDTH, -barFinalHeight);
+    ctx.fillText(players[i], barPos, textY);
+    ctx.fillText(Math.floor(times[i]), barPos, counterY);
+  });
 };
 
 window.renderStatistics = function (ctx, players, times) {
+  // Отрисовка облака
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+  // Отрисовка текста приветствия
+  renderTitle(ctx, titleX, titleY, '#000', '16px PT Mono');
+  renderSubtitle(ctx, titleX, titleY + FONT_HEIGHT, '#000', '16px PT Mono');
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + (GAP * 2), CLOUD_Y + (GAP * 2) + FONT_GAP);
-  ctx.fillText('Список победителей:', CLOUD_X + (GAP * 2), CLOUD_Y + (GAP * 2) + TEXT_HEIGHT + FONT_GAP);
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < players.length; i++) {
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(210, ' + Math.floor(Math.random() * 100) + '%, 50%)';
-    }
-
-    var barFinalHeight = (barHeight * times[i]) / maxTime;
-    var counterY = CLOUD_HEIGHT - (barFinalHeight + GAP + TEXT_HEIGHT + FONT_GAP);
-
-    ctx.fillRect(CLOUD_X + MARGIN + (BAR_WIDTH + BAR_GAP) * i, barY, BAR_WIDTH, -barFinalHeight);
-    ctx.fillText(players[i], CLOUD_X + MARGIN + (BAR_WIDTH + BAR_GAP) * i, textY);
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + MARGIN + (BAR_WIDTH + BAR_GAP) * i, counterY);
-  }
+  renderHist(ctx, players, times);
 };
