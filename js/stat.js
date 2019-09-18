9a2d94ed9cec9bf18cd6b1765829f64577851f85
nokia6680 <zenitfan@yandex.ru>
2019-09-18 23:54:13 +0200

    Разобрал

--- js/stat.js
+++ js/stat.js
@@ -1,49 +1,4 @@
 'use strict';
-
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-// Отступ между текстом и границами
-var GAP = 10;
-// Ширина колонки
-var BAR_WIDTH = 40;
-// Расстояние между колонками
-var BAR_GAP = 50;
-// Горизонтальный отступ гистограммы от границы облака
-var MARGIN = 55;
-// высота текста
-var FONT_HEIGHT = 15;
-// Отступ от текста
-var FONT_GAP = 10;
-// Цвет бара и текста гл.игрока
-var MY_COLOR = 'rgba(250, 0, 0, 1)';
-// Позиция текста вертикальная
-var textY = CLOUD_HEIGHT - GAP;
-// "Шаг" бара гистограммы по горизонтали
-var barStep = BAR_WIDTH + BAR_GAP;
-// Позиция бара гистограммы по горизонтали
-var barX = CLOUD_X + MARGIN;
-// Позиция бара вертикальная
-var barY = CLOUD_HEIGHT - GAP - FONT_HEIGHT - FONT_GAP / 2;
-// Расчет максимальной высоты гистограммы
-var barHeight = CLOUD_HEIGHT - GAP * 2 - (FONT_GAP + FONT_HEIGHT) * 4;
-// Позиция поздравления горизонтальная
-var titleX = CLOUD_X + (GAP * 3);
-// Позиция поздравления вертикальная
-var titleY = CLOUD_Y + (GAP * 3);
-// Текст заголовка при победе
-var titleText = 'Ура вы победили!';
-// Текст подзаголовка при победе
-var subTitleText = 'Список победителей:';
-
-
-// Функция для рендера облака
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
-
 // Функция поиска максимального значения
 function getMaxElement(arr) {
   return Math.max.apply(null, arr);
@@ -53,41 +8,87 @@ var makeRandomColor = function () {
   return ('hsl(240, ' + (Math.random() * 100) + '%, 50%)');
 };
 
-// Функция отрисовки текста поздравления
-var renderTitle = function (ctx, x, y, color, font) {
-  ctx.fillStyle = color;
-  ctx.font = font;
-  ctx.fillText(titleText, x, y);
-};
-// Функция отрисовки текста вывода списка победителей
-var renderSubtitle = function (ctx, x, y, color, font) {
-  ctx.fillStyle = color;
-  ctx.font = font;
-  ctx.fillText(subTitleText, x, y);
-};
-var renderHist = function (ctx, players, times) {
-  var maxTime = getMaxElement(times);
-
-  players.forEach(function (name, i) {
-    var barColor = name === 'Вы' ? MY_COLOR : makeRandomColor();
-    var barFinalHeight = ((barHeight * times[i]) / maxTime) * -1;
-    var barPos = barX + (barStep * i);
-    var counterY = CLOUD_HEIGHT - (barFinalHeight + GAP + FONT_HEIGHT + FONT_GAP);
-
-    ctx.fillStyle = barColor;
-    ctx.fillRect(barPos, barY, BAR_WIDTH, barFinalHeight);
-    ctx.fillText(players[i], barPos, textY);
-    ctx.fillText(Math.floor(times[i]), barPos, counterY);
-  });
-};
-
 window.renderStatistics = function (ctx, players, times) {
-  // Отрисовка облака
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  // Отрисовка текста приветствия
-  renderTitle(ctx, titleX, titleY, '#000', '16px PT Mono');
-  renderSubtitle(ctx, titleX, titleY + FONT_HEIGHT, '#000', '16px PT Mono');
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var COLUMN_WIDTH = 40;
 
-  renderHist(ctx, players, times);
+  // Функция для рендера облака (координаты, цвет)
+  var renderCloud = function (x, y, color) {
+    var CLOUD_WIDTH = 420;
+    var CLOUD_HEIGHT = 270;
+
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  };
+
+  var renderCloudWithShadow = function () {
+    var GAP = 10; // Отступ от края облака
+    var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)'; // Цвет тени
+    var CLOUD_COLOR = '#ffffff'; // Цвет облака
+    // Передали координаты для облака с тенью + цвет, так же для просто облака
+    renderCloud(CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
+    renderCloud(CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  };
+  // Функция отрисовки текста
+  var renderText = function (strings, color, x, y) {
+    var FONT_PROPERTIES = ['16px', 'PT Mono']; // Массив параметров текста
+    var FONT_HEIGHT = 15;
+    var BASIC_FONT_COLOR = '#000';
+    var TITLE_X = CLOUD_X + 50;
+    var TITLE_Y = 40;
+    x = x || TITLE_X; // С оператором сравнения "или"
+    y = y || TITLE_Y;
+
+    ctx.font = FONT_PROPERTIES.join(' '); // Объединение в строку элементов массива
+    ctx.fillStyle = color || BASIC_FONT_COLOR;
+    strings.forEach(function (string, i) {
+      ctx.fillText(string, x, y + (FONT_HEIGHT * i));
+    });
+  };
+  // Отрисовка текста приветствия
+  var renderTopText = function () {
+    var TOP_TEXT = ['Ура вы победили!', 'Список победителей']; // Соответственно, после пересчета количества элементов массива отрисовка нарисует 2 строки вертикально с отступом FONT_HEIGHT
+    renderText(TOP_TEXT);
+  };
+
+  var renderColumn = function (left, bottom, columnHeight, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(left, bottom - columnHeight, COLUMN_WIDTH, columnHeight);
+  };
+
+  var renderHist = function (columnHeight, left, name, time) {
+    var BAR_Y = 255; // CLOUD_HEIGHT - GAP - FONT_HEIGHT
+    var userTimeY = BAR_Y - columnHeight - 10; // Чтобы координата была вверху - отрицательное число = высоте отрисованного бара + отступ между текстом и баром
+    var userNameY = BAR_Y + 15; // Высота бара + высота текста
+    var MY_COLOR = 'rgba(255, 0, 0, 1)'; // Красный цвет бара игрока
+    var color = name === 'Вы' ? MY_COLOR : makeRandomColor(); // Если имя = "Вы", то цвет - красный, иначе - вызываем функцию генерации случайной насыщенности синего цвета
+    time = Math.round(time);
+    renderText([name], color, left, userNameY);
+    renderText([time], color, left, userTimeY);
+    renderColumn(left, BAR_Y, columnHeight, color);
+  };
+
+  var renderHistogram = function () {
+    var COLUMN_HEIGHT = 150;
+    var CLOUD_MARGIN = 55;
+    var barX = CLOUD_X + CLOUD_MARGIN; // Стартовая позиция
+    var BAR_GAP = 50;
+    var barStep = COLUMN_WIDTH + BAR_GAP; // Шаг бара
+    var maxTime = getMaxElement(times);
+    players.forEach(function (name, i) {
+      var time = times[i];
+      var columnHeight = times[i] / maxTime * COLUMN_HEIGHT; // Расчитанная высота бара
+      var currentColumnX = barX + i * barStep; // Текущая гор.координата бара
+      renderHist(columnHeight, currentColumnX, name, time);
+    });
+  };
+
+  var init = function () {
+    renderCloudWithShadow();
+    renderTopText();
+    renderHistogram();
+  };
+
+  init();
 };
